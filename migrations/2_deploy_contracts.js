33ac68fd9ccbfcbbfc2977682156d130aa991641
guangyusong <guangysong@gmail.com@users.noreply.github.com>
2020-05-04 00:59:35 -0400

    Fixed issue in not deploying constructor

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var Purchase = artifacts.require("./Purchase.sol");
+var Activate = artifacts.require("./Activate.sol");
 
 module.exports = function (deployer) {
-    deployer.deploy(Purchase);
+    deployer.deploy(Purchase).then(function () {
+        return deployer.deploy(Activate, Purchase.address);
+    })
 };
--- src/downloads/program.py
+++ src/downloads/program.py
@@ -3,6 +3,11 @@ from web3 import Web3
 
 set_solc_version('v0.5.0')
 
+# Contract Addresses. This needs to be manually configured.
+activate_address = "0x77Ba6ECB94c9203Ef1E36b76733683764065B353"
+purchase_address = "0xe7bF216b67aFb261C8D41fCBDd4153045234e0F8"
+
+
 # Find Ganache Instance
 # In the real world. This would be the address for the test network
 GANACHE = "http://127.0.0.1:7545"
@@ -14,49 +19,54 @@ w3 = Web3(Web3.HTTPProvider(GANACHE))
 main_account = w3.eth.accounts[0]
 w3.eth.defaultAccount = main_account
 
-# In the real world we won't need this
+# In the real world we will be interacting with the blockchain directly
+# and will not need this
+""" Contract: Activate.sol """
+# Directory to our contracts
 compiled_sol = compile_files(
-    ["./contracts/Purchase.sol"])  # Compiled source code
+    ["./certify/contracts/Activate.sol"])
 
 # dictionary with compiled code for a single contract
 compiled_sol_keys = list(compiled_sol.keys())
 
 contract_interface = compiled_sol.get(compiled_sol_keys[0])
 
-contract_address = "0xf442BdB194b91625C2628532834a445f41Cd9AA9"
-
 # instantiate the contract, with specific contract address
-purchase_contract = w3.eth.contract(
-    address=contract_address,
+activate_contract = w3.eth.contract(
+    address=activate_address,
     abi=contract_interface['abi'],
     bytecode=contract_interface['bin'])
 
-# execute a call, which does not execute a transaction (i.e. no write => no gas)
+""" Contract: Purchase.sol"""
+# Directory to our contracts.
+compiled_sol2 = compile_files(
+    ["./certify/contracts/Purchase.sol"])
+
+# dictionary with compiled code for a single contract
+compiled_sol_keys2 = list(compiled_sol2.keys())
+
+contract_interface2 = compiled_sol2.get(compiled_sol_keys2[0])
+
+# instantiate the contract, with specific contract address
+purchase_contract = w3.eth.contract(
+    address=purchase_address,
+    abi=contract_interface2['abi'],
+    bytecode=contract_interface2['bin'])
 
 
+# Helper Functions from Sections
 def exec_call(w3, contract_inst, function_name, *f_args):
     func_inst = contract_inst.get_function_by_name(function_name)
-
     return_value = func_inst(*f_args).call()
     return return_value
-
-# execute a transaction (i.e. a write), and return the transaction receipt (costs gas paid in Ether)
 
 
 def exec_transact_receipt(w3, contract_inst, function_name, *f_args):
     func_inst = contract_inst.get_function_by_name(function_name)
-
-    # get the return value, without executing transaction
     return_value = exec_call(w3, contract_inst, function_name, *f_args)
-
-    # execute the transaction
     tx_hash = func_inst(*f_args).transact()
-    # receipt does not contain values returned by function
     tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-
     return return_value, tx_receipt
-
-# execute transaction, but ignore the receipt
 
 
 def exec_transact(w3, contract_inst, function_name, *f_args):
@@ -64,12 +74,13 @@ def exec_transact(w3, contract_inst, function_name, *f_args):
     return rv
 
 
+# Check license
 def check_license():
     # Part 1: Verify that the transaction hash exist
     while True:
         try:
             transaction_hash = input("Please enter your license key: ")
-            print("Thank you. Checking...")
+            print("Thank you. Checking for license key validity...")
             transaction = w3.eth.getTransaction(transaction_hash)
             print("Your license key is valid.")
             # Get the account that sent the transaction
@@ -82,27 +93,28 @@ def check_license():
     while True:
         try:
             account in w3.eth.accounts
-            print("Thank you. Checking...")
+            print("Thank you. Comparing account info with our database...")
             break
         except:
             print(
                 "Your account info does not match our database. Software will not activate.")
 
+    # Load
     # Get transaction receipts from the hash / our license
     receipt = w3.eth.getTransactionReceipt(transaction_hash)
-    # Get Events from the transaction receipt
+    # Get Events from the transaction receipt on our main contract
     logs = purchase_contract.events.transactionEvent().processReceipt(receipt)
-    # Get our sale ID
+    # Get our sale ID from the event
     saleId = logs[0].args._id
 
     # Part 3: Verify that the license has not been activated
-    checkActive = purchase_contract.functions.isActive(saleId).call()
+    checkActive = activate_contract.functions.isActive(saleId).call()
     if False == checkActive:
         choice = input(
-            "The software has not been activated, would you like to activate it? (y/n)")
+            "The software has not been activated, would you like to activate it? (y/n): \n")
         if choice == 'y':
             try:
-                exec_transact(w3, purchase_contract,
+                exec_transact(w3, activate_contract,
                               'activateProduct', saleId, transaction_hash)
                 print("Congratulations, the software is now activated.")
                 input("Type anything to exit... \n")
@@ -113,7 +125,7 @@ def check_license():
                 "User chose to terminate the activation process. Software not activated.")
             input("Type anything to exit... \n")
     elif True == checkActive:
-        print("Software is already activated!!")
+        print("Key is invalid...Please purchase a new key.")
     else:
         print("An unknown error has occured. The program will now terminate.")
 
